12298a62ed4d4149428ea139d41150b6b8e02bb5
David Rivers <dmr41.rivers@gmail.com>
2015-05-28 18:01:08 +0200

    added rspec test for set_array method and all assoc sub-methods for ArrayExample class

--- spec/array_examples_spec.rb
+++ spec/array_examples_spec.rb
@@ -1,21 +1,73 @@
 require 'spec_helper'
 require './array_examples'
 
-describe ArrayExamples do
+##
+describe "evaluate_array method" do
 
-	it "fails to set first array and returns default empty array" do
+	it "returns array if input value an array" do
+		arrays_reference = ArrayExamples.new
+		test_array_state = arrays_reference.evaluate_array([1, 2, 3, 6])
+		expect(test_array_state).to eq([1, 2, 3, 6])
+		test_array_state = arrays_reference.evaluate_array([1, "stringy", 3, 6])
+		expect(test_array_state).to eq("Array does not only contain integers.")
+	end
+
+	it "fails to return array if input array contains non-integers" do
+		arrays_reference = ArrayExamples.new
+		test_array_state = arrays_reference.evaluate_array([1, 2, 3, 6])
+		expect(test_array_state).to eq([1, 2, 3, 6])
+		test_array_state = arrays_reference.evaluate_array([1, "stringy", 3, 6])
+		expect(test_array_state).to eq("Array does not only contain integers.")
+	end
+
+	it "fails to return array if input value is not an array" do
+		arrays_reference = ArrayExamples.new
+
+		test_array_state = arrays_reference.evaluate_array("I am words")
+		expect(test_array_state).to eq("Input not an array.")
+
+		test_array_state = arrays_reference.evaluate_array(590999)
+		expect(test_array_state).to eq("Input not an array.")
+	end
+
+end
+
+##
+describe "integers_only_array method" do
+
+	it "sets the first_array instance to the input integer array" do
+		array_reference = ArrayExamples.new
+		expect(array_reference.first_array).to eq([])
+		number_array = array_reference.integers_only_array([1, 2, 3, 4])
+		expect(array_reference.first_array).to eq([1, 2, 3, 4])
+	end
+
+	it "fails to set first_array if any values in input are non-integers" do
+		array_reference = ArrayExamples.new
+		expect(array_reference.first_array).to eq([])
+		number_array = array_reference.integers_only_array([1, "word", 3, 4])
+		expect(number_array).to eq("Array does not only contain integers.")
+		expect(array_reference.first_array).to eq([])
+	end
+
+end
+
+##
+describe "set_array method" do
+
+	it "returns empty array by default" do
 		arrays_reference = ArrayExamples.new
 		empty_array = arrays_reference.set_array
 		expect(empty_array).to match_array([])
 	end
 
-	it "sets first array returns input array" do
+	it "returns input array given" do
 		arrays_reference = ArrayExamples.new
-		empty_array = arrays_reference.set_array([1, 2, 3])
-		expect(empty_array).to match_array([1, 2, 3])
+		number_array = arrays_reference.set_array([1, 2, 3])
+		expect(number_array).to match_array([1, 2, 3])
 	end
 
-	it "fails to set array if input is not an array" do
+	it "returns empty array if input is not an array" do
 		arrays_reference = ArrayExamples.new
 		string_value = arrays_reference.set_array("I like turtles")
 		expect(string_value).to eq([])
@@ -31,10 +83,10 @@ describe ArrayExamples do
 		expect(mixed_array_type).to eq([])
 	end
 
-	it "second array references first array" do
-		arrays_reference = ArrayExamples.new
-		arrays_reference.set_array([1, 2, 3, 4, 5])
-		expect(arrays_reference.second_array_referenced).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
-	end
-
 end
+
+	# it "second array references first array" do
+	# 	arrays_reference = ArrayExamples.new
+	# 	arrays_reference.set_array([1, 2, 3, 4, 5])
+	# 	expect(arrays_reference.second_array_referenced).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+	# end
